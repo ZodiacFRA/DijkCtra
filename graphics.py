2ae1df7c9d18f59f45db0cbce0be7ad2b4a6ef1d
jb <jean-baptiste.dupuy@epitech.eu>
2020-05-15 20:45:40 +0200

    removed graphics.py

--- graphics.py
+++
@@ -1,6 +0,0 @@
-
-
-    def drawBonuses(self):
-        for strPos in s.bonusPoss:
-            pos = strPos.split(',')
-            s.display.blit(s.icons["bonusIcon"], s.getScreenPosition((int(pos[0]), int(pos[1]))))
